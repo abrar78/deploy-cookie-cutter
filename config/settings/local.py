226be50016a96eb2604f6e25d5ea7540e594186b
Ubuntu <azureuser@dpdexample.ub0agfdncerufnto0x1smarcqd.zx.internal.cloudapp.net>
2021-09-25 12:38:27 +0000

    final project

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="MZsQZJnSspOrbn0OcB8m3HZR4zHXSbXGA8u8KQwYWs40nTMH4DF2Ex8LuoH9kpFK",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 # CACHES
 # ------------------------------------------------------------------------------
